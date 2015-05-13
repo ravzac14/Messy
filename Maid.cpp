2f43b2920949e49fa15037172a5c3155d6fad5d9
ravzac14 <raver_zack@yahoo.com>
2015-05-12 23:05:27 -0700

    Expanded the walking animations

--- Maid.cpp
+++ Maid.cpp
@@ -60,21 +60,47 @@ float Maid::getVelocity () const
 void Maid::setFace (ActorFace f)
 {
   mFace = f;
-  std::string newImage = getAnimation();
+  std::string newImage = getWalkAnimation();
   load(newImage);
 }
 
 void Maid::move (float xOffset, float yOffset)
 {
-  if (mFoot == L)
-    mFoot = R;
-  else
-    mFoot = L;
-  std::string newImage = getAnimation();
+  changeFooting();
+  std::string newImage = getWalkAnimation();
   load(newImage);
  
   if (isLegalMove(getPosition().x + xOffset, getPosition().y + yOffset)) 
     getSprite().move(xOffset,yOffset);
+}
+
+void Maid::changeFooting ()
+{
+  if (mFoot == LL) { // --LL
+    mFoot = LN;
+    mPreviousFoot = LL;
+  } else if (mFoot == LN && mPreviousFoot == LL) { // --LN --LL
+    mFoot = N;
+    mPreviousFoot = LN;
+  } else if (mFoot == LN && mPreviousFoot == N) { // --LN --N
+    mFoot = LL;
+    mPreviousFoot = LN;
+  } else if (mFoot == N && mPreviousFoot == LN) { // --N --LN
+    mFoot = RN;
+    mPreviousFoot = N;
+  } else if (mFoot == N && mPreviousFoot == RN) { // --N --RN
+    mFoot = LN;
+    mPreviousFoot = N;
+  } else if (mFoot == RN && mPreviousFoot == N) { // --RN --N
+    mFoot = RR;
+    mPreviousFoot = RN;
+  } else if (mFoot == RN && mPreviousFoot == RR) { // --RN --RR
+    mFoot = N;
+    mPreviousFoot = RN;
+  } else if (mFoot == RR) { // --RR
+    mFoot = RN;
+    mPreviousFoot = RR;
+  }
 }
 
 bool Maid::isLegalMove(float newX, float newY)
@@ -83,57 +109,94 @@ bool Maid::isLegalMove(float newX, float newY)
       //TODO: && getLevelManager().isInLevelBounds(newX,newY,WIDTH,HEIGHT)
 }
 
-std::string Maid::getAnimation ()
+std::string Maid::getWalkAnimation ()
 {
   switch (mFace) {
     case UP: {
       switch (mFoot) {
-        case L: {
-          return UP_L;
+        case LL: {
+          return UP_LL;
         } break;
-        case R: {
-          return UP_R;
+        case LN: {
+          return UP_LN;
+        } break;
+        case N: {
+          return UP_N;
+        } break;
+        case RN: {
+          return UP_RN;
+        } break;
+        case RR: {
+          return UP_RR;
         } break;
       } //switch mFoot
     } break;
     case DOWN: {
       switch (mFoot) {
-        case L: {
-          return DOWN_L;
+        case LL: {
+          return DOWN_LL;
         } break;
-        case R: {
-          return DOWN_R;
+        case LN: {
+          return DOWN_LN;
+        } break;
+        case N: {
+          return DOWN_N;
+        } break;
+        case RN: {
+          return DOWN_RN;
+        } break;
+        case RR: {
+          return DOWN_RR;
         } break;
       } //switch mFoot
     } break;
     case LEFT: {
       switch (mFoot) {
-        case L: {
-          return LEFT_L;
+        case LL: {
+          return LEFT_LL;
         } break;
-        case R: {
-          return LEFT_R;
+        case LN: {
+          return LEFT_LN;
+        } break;
+        case N: {
+          return LEFT_N;
+        } break;
+        case RN: {
+          return LEFT_RN;
+        } break;
+        case RR: {
+          return LEFT_RR;
         } break;
       } //switch mFoot
     } break;
     case RIGHT: {
       switch (mFoot) {
-        case L: {
-          return RIGHT_L;
+        case LL: {
+          return RIGHT_LL;
         } break;
-        case R: {
-          return RIGHT_R;
+        case LN: {
+          return RIGHT_LN;
+        } break;
+        case N: {
+          return RIGHT_N;
+        } break;
+        case RN: {
+          return RIGHT_RN;
+        } break;
+        case RR: {
+          return RIGHT_RR;
         } break;
       } //switch mFoot
     } break;
   } //switch mFace
-} //getAnimation
+} //getWalkAnimation
 
 void Maid::initializeActor ()
 {
   setInitialized(true);
   mFace = DOWN;
-  mFoot = L;
-  std::string newImage = getAnimation();
+  mFoot = N;
+  mPreviousFoot = RN;
+  std::string newImage = getWalkAnimation();
   load(newImage);
 }
--- Maid.h
+++ Maid.h
@@ -10,12 +10,13 @@ class Maid : public IVisibleGameObject
 {
   public:
     enum ActorFace {UP,DOWN,LEFT,RIGHT};
-    enum ForwardFoot {L,R};
+    enum ForwardFoot {LL,LN,N,RN,RR};
 
     Maid();
     ~Maid();
    
     ActorFace getFace ();
+    void changeFooting();
     void setFace(ActorFace f);
     bool isLegalMove(float newX, float newY);
     void move(float xOffset,float yOffset);
@@ -24,22 +25,57 @@ class Maid : public IVisibleGameObject
     void handleKey();
 
     float getVelocity() const;
-    std::string getAnimation ();
-    const std::string UP_L = "assets/100x100-elephant_maid-U-L.png";
-    const std::string UP_R = "assets/100x100-elephant_maid-U-R.png";
-    const std::string DOWN_L = "assets/100x100-elephant_maid-D-L.png";
-    const std::string DOWN_R = "assets/100x100-elephant_maid-D-R.png";
-    const std::string LEFT_L = "assets/100x100-elephant_maid-L-D.png";
-    const std::string LEFT_R = "assets/100x100-elephant_maid-L-U.png";
-    const std::string RIGHT_L = "assets/100x100-elephant_maid-R-U.png";
-    const std::string RIGHT_R = "assets/100x100-elephant_maid-R-D.png";
+    std::string getWalkAnimation ();
+    std::string getIdleAnimation ();
+    std::string getCleanAnimation ();
     const float MAX_VELOCITY = 10.0;
     const float WIDTH = 100.0f;
     const float HEIGHT = 100.0f;
-
+    const std::string UP_LL = "assets/maid_animations/100x100-maid-U-LL.png";
+    const std::string UP_LN = "assets/maid_animations/100x100-maid-U-LN.png";
+    const std::string UP_N = "assets/maid_animations/100x100-maid-U-N.png";
+    const std::string UP_RN = "assets/maid_animations/100x100-maid-U-RN.png";
+    const std::string UP_RR = "assets/maid_animations/100x100-maid-U-RR.png";
+    const std::string DOWN_LL = "assets/maid_animations/100x100-maid-D-LL.png";
+    const std::string DOWN_LN = "assets/maid_animations/100x100-maid-D-LN.png";
+    const std::string DOWN_N = "assets/maid_animations/100x100-maid-D-N.png";
+    const std::string DOWN_RN = "assets/maid_animations/100x100-maid-D-RN.png";
+    const std::string DOWN_RR = "assets/maid_animations/100x100-maid-D-RR.png";
+    const std::string LEFT_LL = "assets/maid_animations/100x100-maid-L-DD.png";
+    const std::string LEFT_LN = "assets/maid_animations/100x100-maid-L-DN.png";
+    const std::string LEFT_N = "assets/maid_animations/100x100-maid-L-N.png";
+    const std::string LEFT_RN = "assets/maid_animations/100x100-maid-L-UN.png";
+    const std::string LEFT_RR = "assets/maid_animations/100x100-maid-L-UU.png";
+    const std::string RIGHT_LL = "assets/maid_animations/100x100-maid-R-UU.png";
+    const std::string RIGHT_LN = "assets/maid_animations/100x100-maid-R-UN.png";
+    const std::string RIGHT_N = "assets/maid_animations/100x100-maid-R-N.png";
+    const std::string RIGHT_RN = "assets/maid_animations/100x100-maid-R-DN.png";
+    const std::string RIGHT_RR = "assets/maid_animations/100x100-maid-R-DD.png";
+    const std::string CLEAN_0 = "assets/maid_animations/100x100-maid-clean-0and11.png";
+    const std::string CLEAN_1 = "assets/maid_animations/100x100-maid-clean-1and10.png";
+    const std::string CLEAN_2 = "assets/maid_animations/100x100-maid-clean-2and4and6and8.png";
+    const std::string CLEAN_3 = "assets/maid_animations/100x100-maid-clean-3and7.png";
+    const std::string CLEAN_4 = "assets/maid_animations/100x100-maid-clean-2and4and6and8.png";
+    const std::string CLEAN_5 = "assets/maid_animations/100x100-maid-clean-5and9.png";
+    const std::string CLEAN_6 = "assets/maid_animations/100x100-maid-clean-2and4and6and8.png";
+    const std::string CLEAN_7 = "assets/maid_animations/100x100-maid-clean-3and7.png";
+    const std::string CLEAN_8 = "assets/maid_animations/100x100-maid-clean-2and4and6and8.png";
+    const std::string CLEAN_9 = "assets/maid_animations/100x100-maid-clean-5and9.png";
+    const std::string CLEAN_10 = "assets/maid_animations/100x100-maid-clean-1and10.png";
+    const std::string CLEAN_11 = "assets/maid_animations/100x100-maid-clean-0and11.png";
+    const std::string IDLE_0 = "assets/maid_animations/100x100-maid-idle-0and5and7.png";
+    const std::string IDLE_1 = "assets/maid_animations/100x100-maid-idle-1and3.png";
+    const std::string IDLE_2 = "assets/maid_animations/100x100-maid-idle-2.png";
+    const std::string IDLE_3 = "assets/maid_animations/100x100-maid-idle-1and3.png";
+    const std::string IDLE_4 = "assets/maid_animations/100x100-maid-idle-4.png";
+    const std::string IDLE_5 = "assets/maid_animations/100x100-maid-idle-0and5and7.png";
+    const std::string IDLE_6 = "assets/maid_animations/100x100-maid-idle-6.png";
+    const std::string IDLE_7 = "assets/maid_animations/100x100-maid-idle-0and5and7.png";
+  
   private:
     ActorFace mFace;
     ForwardFoot mFoot;
+    ForwardFoot mPreviousFoot;
     float mVelocity;
 };
 
